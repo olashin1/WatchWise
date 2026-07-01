bda909e2f965fee3e417fcaf477eae36ff1606a6
Omar <omarlashin29@gmail.com>
2026-06-30 18:41:18 -0700

    cleaned up unused files

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,4 @@
 from fastapi import FastAPI
-from routers.user import router as UserRouter
-from routers.auth import router as AuthRouter
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -18,13 +16,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/")
-def default():
-    return {"message": "Hello World"}
-
 @app.get("/health")
 def health():
     return {"message": "Hello from health"}
-
-app.include_router(UserRouter)
-app.include_router(AuthRouter)
